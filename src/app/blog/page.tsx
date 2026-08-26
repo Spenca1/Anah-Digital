ff8af43a01ca284a087df00ac63c7adae519b333
Anah Uche <anahthankgod@gmail.com>
2026-08-26 08:18:32 +0100

    Fix Blob images and favicon

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,19 +1,17 @@
 import { prisma } from "@/lib/prisma";
 import BlogClient from "./BlogClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function BlogPage() {
+  const posts = await prisma.post.findMany({
+    where: {
+      published: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
-const posts = await prisma.post.findMany({
-where:{
-published:true,
-},
-orderBy:{
-createdAt:"desc",
-},
-});
-
-return (
-<BlogClient posts={posts}/>
-);
-
+  return <BlogClient posts={posts} />;
 }
